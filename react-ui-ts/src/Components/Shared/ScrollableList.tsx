4530a089dfe69d5e1f40fb994ffa79d98e2ebedb
fatimaosman99 <54881403+fatimaosman99@users.noreply.github.com>
2022-10-25 09:50:28 +0200

    Merge branch 'main' into pseudomain

---
+++ react-ui-ts/src/Components/Shared/ScrollableList.tsx
@@ -0,0 +1,13 @@
+type Props = {
+    displayList : JSX.Element[]
+}
+export const ScrollableList = ({displayList } : Props) => {
+    return (<>
+        {displayList.map((displayElement: JSX.Element) => { 
+            return (displayElement)
+            
+        })}
+    </>
+
+    )
+}
