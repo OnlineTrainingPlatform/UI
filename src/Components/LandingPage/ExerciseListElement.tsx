f500dc98b727f189915bb925d26fbbe9b495bb66
fatimaosman99 <54881403+fatimaosman99@users.noreply.github.com>
2022-10-28 13:19:54 +0200

    feat: add event handling to button

--- src/Components/LandingPage/ExerciseListElement.tsx
+++ src/Components/LandingPage/ExerciseListElement.tsx
@@ -1,9 +1,12 @@
 import {ChartBar, Play} from 'phosphor-react';
+import { useNavigate } from "react-router-dom";
+import { MouseEvent } from 'react';
 type Props = { exercise: string, description:string };
 export const ExerciseListElement = ({ exercise, description }: Props) => {
-const handleClick = () => {
-  console.log("Click!")
-}
+  let navigate = useNavigate();
+  const handleClick = (e: MouseEvent<HTMLElement>) => {
+    e.currentTarget.id === "exercise" ? navigate("exercise") : navigate("statistics");
+  }
   return (
           <div className="p-1 border-solid grid grid-cols-2 divide-x">
             <div>
@@ -11,8 +14,8 @@ const handleClick = () => {
               <p>{description}</p>
             </div>
             <div className="space-x-4">
-            <button onClick={handleClick} className="bg-white hover:bg-gray-100 text-gray-800"><Play size={32} /></button>
-            <button onClick={handleClick} className="bg-white hover:bg-gray-100 text-gray-800"><ChartBar size={32} /></button>
+            <button id="exercise" onClick={handleClick} className="bg-white hover:bg-gray-100 text-gray-800"><Play size={32} /></button>
+            <button id="statistics" onClick={handleClick} className="bg-white hover:bg-gray-100 text-gray-800"><ChartBar size={32} /></button>
             </div>
           </div>
       );
