debbf80c5b662111435bcb19ebe3dccf5830f7c3
Jakob F Gregersen <lilleper.jfg@gmail.com>
2022-10-28 09:32:10 +0200

    style(Layout): add padding and background color

--- src/Components/Layout/Layout.tsx
+++ src/Components/Layout/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 export const Layout = () => {
   return (
-    <div className="ml-5">
+    <div className="p-10 h-screen bg-gray-100">
       <Outlet />
     </div>
   );
