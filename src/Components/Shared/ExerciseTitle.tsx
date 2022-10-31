881edfc8333fbd0330858fcf500fae3d52fbf9b3
Jakob F Gregersen <lilleper.jfg@gmail.com>
2022-10-31 13:10:11 +0100

    feat: add shared layout component

--- src/Components/Shared/ExerciseTitle.tsx
+++ src/Components/Shared/ExerciseTitle.tsx
@@ -1,8 +1,13 @@
-export const ExerciseTitle = () => {
+interface Props {
+  title: string | undefined; // undefined allowed only because useParams does not guarantee a string.
+  breadcrumb: string;
+}
+
+export const ExerciseTitle = ({ title, breadcrumb }: Props) => {
   return (
     <>
-      <h1>Title</h1>
-      <h2>Subtitle</h2>
+      <h1 className="font-medium">{!title ? 'loading' : title}</h1>
+      <h3 className="font-light">{breadcrumb}</h3>
     </>
   );
 };
