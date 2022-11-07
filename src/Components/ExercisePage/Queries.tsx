6bc7dd75c05ab52ce2231c97c736b08309fac0ff
fatimaosman99 <54881403+fatimaosman99@users.noreply.github.com>
2022-11-07 08:48:11 +0100

    chore: add requested changes

--- src/Components/ExercisePage/Queries.tsx
+++ src/Components/ExercisePage/Queries.tsx
@@ -5,9 +5,6 @@ export const Queries = () => {
   return (
     <div className="flex w-2/6 h-4/6 pr-6 pb-6 pl-6 flex-col">
       <h3 className="flex p-0 m-0 mb-2">Queries</h3>
-      {/* <ScrollableList
-        displayList={!!queryList ? queryList : [<p>Loading...</p>]}
-      /> */}
       <div className="flex flex-row relative">
         <button className="fixed flex right-28 bottom-1">Run queries</button>
         <button className="fixed flex right-1 bottom-1">Submit solution</button>
--- src/Hooks/getAllExercises.ts
+++ src/Hooks/getAllExercises.ts
@@ -6,14 +6,6 @@ const URL_API = 'localhost:8000';
 
 export const useGetAllExercises = () => {
   const [exercises, setExercises] = useState<Exercise[] | undefined>();
-  // fetch(`http://${URL_API}/api/v1/exercises`)
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     setExercises(data);
-  //   })
-  //   .catch((e) => {
-  //     console.error(e);
-  //   });
   if (!exercises) {
     setExercises([
       {
