623dd95dc7f9822ba997ce819a0741cf3dd98414
Jakob F Gregersen <lilleper.jfg@gmail.com>
2022-11-07 09:12:24 +0100

    feat(statistics-page): implement HandInTimeStatistics + PassedStatistics

--- src/Components/StatisticsPage/HandInTimeStatistics.tsx
+++ src/Components/StatisticsPage/HandInTimeStatistics.tsx
@@ -1,8 +1,13 @@
+import dayjs from "dayjs";
+
 export const HandInTimeStatistics = () => {
+  const today = dayjs().format("HH:MM")
   return (
-    <div>
-      <h3>Average hand-in time</h3>
-      <h1>22.00</h1>
+    <div className="border-solid mt-6">
+      <div className="w-full p-auto text-center">
+        <h3>Average hand in time</h3>
+        <h1>{today.toString()}</h1>
+      </div>
     </div>
   );
 };
