57f27207306b5810e4f3ee59632fe791f0047f74
Jakob F Gregersen <lilleper.jfg@gmail.com>
2022-11-25 13:30:03 +0100

    feat: include christmas

--- src/Components/Layout/Layout.tsx
+++ src/Components/Layout/Layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import Snowfall from 'react-snowfall'
 
 export const Layout = () => {
   return (
     <div className="pt-10 pl-10 pb-10 h-screen bg-gray-100">
       <Outlet />
+      <Snowfall color="silver"/>
     </div>
   );
 };
