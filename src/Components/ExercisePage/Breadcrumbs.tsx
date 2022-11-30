80e581e19ae25510927d7cb941430c4156b49d13
Dieuer <Dieuer@users.noreply.github.com>
2022-11-30 23:30:57 +0000

    format: Automatic formatted and linted stuff

--- src/Components/ExercisePage/Breadcrumbs.tsx
+++ src/Components/ExercisePage/Breadcrumbs.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import { Link, NavLink, useParams } from 'react-router-dom';
 
 export const Breadcrumbs = () => {
-
-  const refreshPage = ()=>{
+  const refreshPage = () => {
     window.location.reload();
- }
+  };
 
   return (
     <div>
       <nav>
-        <Link to="/">Home</Link> /
-        <a onClick={refreshPage}>Exercise</a>
+        <Link to="/">Home</Link> /<a onClick={refreshPage}>Exercise</a>
       </nav>
     </div>
   );
--- src/Components/StatisticsPage/Breadcrumbs.tsx
+++ src/Components/StatisticsPage/Breadcrumbs.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 
 export const Breadcrumbs = () => {
-
-  const refreshPage = ()=>{
+  const refreshPage = () => {
     window.location.reload();
- }
+  };
 
   return (
     <div>
       <nav>
-        <Link to="/">Home</Link> /
-        <a onClick={refreshPage}>Statistics</a>
+        <Link to="/">Home</Link> /<a onClick={refreshPage}>Statistics</a>
       </nav>
     </div>
   );
